6caa33c84e3143c08d88c809d0d18393453fd3a9
Azazel0203 <aadarshkr.singh.cd.ece21@itbhu.ac.in>
2024-03-08 17:02:17 +0530

    fixed the prompt

--- src/prompt.py
+++ src/prompt.py
@@ -1,13 +1,14 @@
 prompt_template="""
-Use the following pieces of information and the chat history (delimited by <hs></hs>) to answer the user's question.
+Use the following pieces of information and the chat history to answer the user's question.
 If you don't know the answer, simply state that you don't know; avoid making up an answer.
+Keep the answer short and crisp
 
 Question: {question}
 Information: {context}
+History: {history}
 
-<hs>
-{history}
-</hs>
+If there is nothing in the history, Please ignore it.
+
 
 Helpful Answer:
 """
